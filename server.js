6d2ec9e97762cd27f91b4287fb01a23de0511d49
Vengatesh Murugesan <74056614+Vengatesh-Murugesan@users.noreply.github.com>
2024-05-12 15:43:51 +0530

    port removed

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const port = 6000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(process.env.port, () =>
+    app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
   } catch (error) {
